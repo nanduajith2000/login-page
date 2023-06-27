f8970cede264534493dfa7c80741520ffb7ec0ac
Nandagopal <nandu2k16@gmail.com>
2023-06-27 14:50:19 +0530

    navigation change

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -63,11 +63,10 @@ const LoginForm = () => {
       console.log(res);
 
       if (res.message === "success") {
-
         // console.log(res.token);
-        document.cookie = "user=" + res.token+";";
-        console.log(document.cookie)
-        navigate("/dashboard");
+        document.cookie = "user=" + res.token + ";";
+        console.log(document.cookie);
+        navigate("/home");
       } else alert("Invalid Credentials");
     });
   };
