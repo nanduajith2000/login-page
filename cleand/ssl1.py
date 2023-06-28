4745a8e5da05efc3b4ed82b34d80598e3c20f16b
albertalex <albathattam@gmail.com>
2023-06-28 10:35:22 +0530

    trying out logout

--- cleand/ssl1.py
+++ cleand/ssl1.py
@@ -1,6 +1,6 @@
 import requests
 import time
-import urllib3
+import urllib3.util.ssl_
 import xmltodict
 
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
@@ -18,6 +18,21 @@ def login(URL,head):
       print(data_dict)
       print("The time of execution is :",
             (end-start) * 10**3, "ms")
+
+def logout(token):
+      url = "https://conference.ngn.bsnl.co.in/rest/V3R8C30/logout"
+      headers = {'content-type': 'application/json',
+        'Authorization':'Basic '+ token}
+      # headers.update(head)
+      start = time.time()
+      r = requests.delete(url, verify=False,headers=headers)
+      end = time.time()
+
+      data_dict = xmltodict.parse(r.content)
       
+      print(data_dict)
+      print("The time of execution is :",
+            (end-start) * 10**3, "ms")
       
-      return data_dict
+
+logout('MTE5MTI4MTQyNDMxNjU5NjY2NjkwMDAtMDAxMA==')
