255c301001a61e1660778d258516ee7bb64dbe92
Akshayc002 <akshayc2002@gmail.com>
2023-07-06 03:22:07 +0530

    Conference History API

---
+++ src/api/QueryConferenceHistory.js
@@ -0,0 +1,88 @@
+const url = `http://35.154.233.185:8000/user/conferencelist`;
+function convertUTCMillisecondsToDate(utcMilliseconds) {
+  // Create a new Date object with the UTC milliseconds
+  var date = new Date(utcMilliseconds);
+
+  // Specify the time zone as 'Asia/Kolkata' for Indian time
+  var options = { timeZone: 'Asia/Kolkata' };
+
+  // Extract the different components of the date in Indian time
+  var year = date.toLocaleString('en-IN', { year: 'numeric', options });
+  var month = date.toLocaleString('en-IN', { month: '2-digit', options });
+  var day = date.toLocaleString('en-IN', { day: '2-digit', options });
+  var hours = date.toLocaleString('en-IN', { hour: '2-digit', hour12: false, options });
+  var minutes = date.toLocaleString('en-IN', { minute: '2-digit', options });
+
+  // Format the date and time string
+  var formattedDate = year + '-' + month + '-' + day;
+  var formattedTime = hours + ':' + minutes ;
+
+  // Return the formatted date and time
+  return formattedDate + ' ' + formattedTime;
+}
+
+function convertMillisecondsToHoursAndMinutes(milliseconds) {
+  var hours = Math.floor(milliseconds / (1000 * 60 * 60));
+  var minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
+
+  return { hours: hours, minutes: minutes };
+}
+
+
+function queryConferencehistory(token) {
+  //attendee is a json file
+  return fetch(url, {
+    method: "POST", // Adjust the HTTP method (GET, POST, PUT, etc.) as required by your API
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        token: `${token}`,
+        "filter":{
+          "resultFields":[
+            "StartTime",
+            "Subject",
+            "ConferenceID",
+            "SubConferenceID",
+            "ConferenceState",
+            "Length",
+            "TimeZone",
+            "ScheduserName",
+            "mediaTypes",
+            "accessNumber",
+            "factEndTime",
+            "accountID",
+            "totalSize"], 
+          "conditions":{
+              "key":"ConferenceState",
+              "value":"Destroyed",
+              "matching":"equal"
+          },
+          "isAscend":"False",
+          "pageIndex": 0,
+          "pageSize": 15 },
+          "isIncludeInvitedConference":"True"
+      },
+    ),
+  })
+    .then((response) => response.json()) // Parse the response as JSON
+    .then((data) => {
+      for(var i=1;i<=data['total'];i++){
+          console.log(i)
+          console.log(data[`${i}`].subject)
+          console.log(data[`${i}`].size)
+          console.log(convertMillisecondsToHoursAndMinutes(data[`${i}`].length))
+          const utcTimestamp = parseInt( data[`${i}`].startTime,10);
+          console.log(convertUTCMillisecondsToDate(utcTimestamp))
+          console.log(data[`${i}`].conferenceKey["conferenceID"])
+          console.log(data[`${i}`].accountID)
+          console.log(data[`${i}`].chair)
+          console.log(data[`${i}`].general)
+        }
+
+    });
+}
+
+module.exports = queryConferencehistory;
+
+// queryConferencehistory("MzUxNTU0MTUwNDE3NjE0OTQyMDQwMDAtMDAxMA==")
