aeb346a75fe125cef6f56997bc69cb210c4f20d1
albertalex <albathattam@gmail.com>
2023-07-14 13:24:48 +0530

    endcall

--- cleand/ssl1.py
+++ cleand/ssl1.py
@@ -4,6 +4,7 @@ import urllib3
 import xmltodict
 import json
 import urllib.parse
+from urllib.parse import quote
 
 
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
@@ -40,7 +41,7 @@ def create_POST(URL, head, body):
     
     body_xml = '<?xml version="1.0" encoding="UTF-8"?>' + xmltodict.unparse( body, full_document=False)
     print("\nBody IN\n",body_xml)
-#     print(headers)
+
     start = time.time()
     r = requests.post(url, headers=headers, data=body_xml, verify=False)
     end = time.time()
@@ -69,7 +70,6 @@ def update_PUT(URL, head, body):
     body_xml = '<?xml version="1.0" encoding="UTF-8"?>' + xmltodict.unparse( body, full_document=False)
     print("\nBody IN\n",body_xml)
 
-#     print(headers)
     start = time.time()
     r = requests.put(url, headers=headers, data=body_xml, verify=False)
     end = time.time()
@@ -136,10 +136,13 @@ def data_GET(URL, head):
 def remove_DELETE(URL,head):
     requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
     requests.packages.urllib3.disable_warnings() 
-    url = "https://conference.ngn.bsnl.co.in/rest/V3R8C30/"+URL
+    encoded_url=quote(URL)
+    url = "https://conference.ngn.bsnl.co.in/rest/V3R8C30/"+encoded_url
     headers = {'content-type': 'application/json'}
     headers.update(head)
-    
+    print("\nHeader\n",headers)
+    print("\nURL IN\n",url)
+
     start = time.time()
     r = requests.delete(url,headers=headers, verify=False)
     end = time.time()
